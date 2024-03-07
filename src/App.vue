43dda649f462c148598cc5aaffec5452c72a7c59
Yuri Rebrov <yurirebrov01@gmail.com>
2024-03-07 14:12:29 +0300

    save to localstorage & fetch from localstorage

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,45 @@
 <template>
   <Header />
   <div class="container">
-    <Balance :total="+total"/>
+    <Balance :total="+total" />
     <IncomeExpenses :income="+income" :expense="+expense" />
-    <TransactionList :transactions="transactions" @transactionDeleted="handleTransactionDelete"/>
-    <TransactionForm @transactionSubmitted="handleTransactionSubmit"/>
+    <TransactionList :transactions="transactions" @transactionDeleted="handleTransactionDelete" />
+    <TransactionForm @transactionSubmitted="handleTransactionSubmit" />
   </div>
 </template>
 
 <script setup>
-import { ref, computed } from 'vue';
+import { ref, computed, onMounted } from 'vue';
 import { useToast } from 'vue-toastification';
 
 const toast = useToast()
 
-const transactions = ref([
-  { id: 1, text: 'Flower', amount: -30 },
-  { id: 2, text: 'Salary', amount: 159.63 },
-  { id: 3, text: 'Book', amount: -9.99 },
-  { id: 4, text: 'Camera', amount: 149.99 }
-]);
+const transactions = ref([]);
 
+onMounted(() => {
+  const savedTransactions = JSON.parse(localStorage.getItem('transactions'))
+  if (savedTransactions) transactions.value = savedTransactions
+})
 
 const total = computed(() => {
-    return transactions.value.reduce((acc, transaction) => {
-        return acc + transaction.amount
-    }, 0)
+  return transactions.value.reduce((acc, transaction) => {
+    return acc + transaction.amount
+  }, 0)
 })
 
 const income = computed(() => {
-    return transactions.value
+  return transactions.value
     .filter((transaction) => transaction.amount > 0)
     .reduce((acc, transaction) => {
-        return acc + transaction.amount
+      return acc + transaction.amount
     }, 0).toFixed(2)
 })
 
 const expense = computed(() => {
-    return transactions.value
+  return transactions.value
     .filter((transaction) => transaction.amount < 0)
     .reduce((acc, transaction) => {
-        return acc + transaction.amount
+      return acc + transaction.amount
     }, 0).toFixed(2)
 })
 
@@ -50,13 +49,19 @@ const handleTransactionSubmit = (transactionData) => {
     text: transactionData.text,
     amount: transactionData.amount
   })
+  saveTransactions()
 
   toast.success('Операция успешно добавлена!✨')
 }
 
 const handleTransactionDelete = (id) => {
   transactions.value = transactions.value.filter((transaction) => transaction.id !== id)
+  saveTransactions()
   toast.success('Операция была успешно удалена!✏️')
+}
+
+const saveTransactions = () => {
+  localStorage.setItem('transactions', JSON.stringify(transactions.value))
 }
 </script>
 
